697a0288ae4c83a3e884f0ffd757305dfaffa667
Michael Liao <michael_5608@hotmail.com>
2017-10-24 16:20:14 -0700

    Updated Junit Test
    Signed-off-by: Michael Liao <michael_5608@hotmail.com>

--- objects/Tests/StudentTest.java
+++ objects/Tests/StudentTest.java
@@ -2,6 +2,12 @@ import static org.junit.Assert.*;
 
 public class StudentTest {
     @org.junit.Test
+    public void getID() throws Exception {
+        Student stu = new Student("Rodney", "Tran");
+        assertEquals(null, stu.getID());
+    }
+
+    @org.junit.Test
     public void getFirstName() throws Exception {
         Student stu = new Student("Rodney", "Tran");
         assertEquals("Rodney", stu.getFirstName());
@@ -19,4 +25,33 @@ public class StudentTest {
         assertEquals("Rodney Tran", stu.getName());
     }
 
+    @org.junit.Test
+    public void getPriority() throws Exception {
+        Student stu = new Student("Rodney", "Tran");
+        assertEquals(0, stu.getPriority());
+    }
+
+    @org.junit.Test
+    public void getGPA() throws Exception {
+        Student stu = new Student("Rodney", "Tran");
+        assertEquals(null, stu.getGPA());
+    }
+
+    @org.junit.Test
+    public void getStudentChoices() throws Exception {
+        Student stu = new Student("Rodney", "Tran");
+        assertEquals(null, stu.getStudentChoices());
+    }
+
+    @org.junit.Test
+    public void getAssignedOption() throws Exception {
+        Student stu = new Student("Rodney", "Tran");
+        assertEquals(null, stu.getAssignedOption());
+    }
+
+    @org.junit.Test
+    public void getStatus() throws Exception {
+        Student stu = new Student("Rodney", "Tran");
+        assertEquals(null, stu.getStatus());
+    }
 }
