3a5750327792bb47b5d6eb54a1f297d1304c95ca
Michael Liao <michael_5608@hotmail.com>
2017-10-27 10:34:35 -0700

    Fixed and added more functional testing of different objects

--- objects/Tests/StudentPlacementTest.java
+++ objects/Tests/StudentPlacementTest.java
@@ -1,3 +1,11 @@
-public class StudentPlacementTest {
+import java.util.ArrayList;
+import static org.junit.Assert.*;
 
+public class StudentPlacementTest {
+    ArrayList<Student> students = new ArrayList<>();
+
+    @org.junit.Test
+    public void getPosition() throws Exception {
+
+    }
 }
